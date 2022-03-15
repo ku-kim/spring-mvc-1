0936ddf999602e4d31041484f2b5efdfb8f626fb
“kukim” <kukim.dev@gmail.com>
2022-03-15 10:38:09 +0900

    서블릿,JSP,MVC패턴: 회원 관리 웹 애플리케이션 요구사항 예시(도메인, 리포지토리만 구현)
    - 도메인 Member
    - 리포지토리 MemberRepository, 테스트 MemberRepositoryTest 구현

---
+++ src/test/java/hello/servlet/basic/domain/mamber/MemberRepositoryTest.java
@@ -0,0 +1,44 @@
+package hello.servlet.basic.domain.mamber;
+
+import org.junit.jupiter.api.AfterEach;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+
+import java.util.List;
+
+import static org.assertj.core.api.Assertions.assertThat;
+import static org.junit.jupiter.api.Assertions.*;
+
+@DisplayName("MemberRepositoryTest 클래스")
+class MemberRepositoryTest {
+
+    MemberRepository memberRepository = MemberRepository.getInstance();
+
+    @AfterEach
+    void tearDown() {
+        memberRepository.clear();
+    }
+
+    @Test
+    void save() {
+        Member member = new Member("hello", 20);
+
+        Member saveMember = memberRepository.save(member);
+
+        Member findMember = memberRepository.finedById(saveMember.getId());
+        assertThat(findMember).isEqualTo(saveMember);
+    }
+
+    @Test
+    void findAll() {
+        Member member1 = new Member("hello1", 20);
+        Member member2 = new Member("hello2", 20);
+        memberRepository.save(member1);
+        memberRepository.save(member2);
+
+        List<Member> result = memberRepository.findAll();
+
+        assertThat(result.size()).isEqualTo(2);
+        assertThat(result).contains(member1, member2);
+    }
+}

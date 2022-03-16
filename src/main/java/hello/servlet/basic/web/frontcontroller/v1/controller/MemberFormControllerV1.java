c01f83fa1aa81e4372e0886ba85cef65bc6f9c82
“kukim” <kukim.dev@gmail.com>
2022-03-16 21:42:41 +0900

    MVC프레임워크 만들기: 프론트 컨트롤러 도입 (v1)
    - 기존에는 컨트롤러마다 서블릿이 있었다면 지금은 FrontControllerServletV1에서만 한 번 쓰이고, 모든 URL 패턴을 받아온다. (urlPatterns = "/front-controller/v1/*")
    - FrontControllerServletV1 에서 ControllerV1 인터페이스를 맵으로 가지고 있고 url에 따라 해당 ControllerV1의 구현체들을 찾아 실행한다.
    - 각 컨트롤러는 ControllerV1 인터페이스를 구현체로 다형성 활용하여 구현된다.

---
+++ src/main/java/hello/servlet/basic/web/frontcontroller/v1/controller/MemberFormControllerV1.java
@@ -0,0 +1,20 @@
+package hello.servlet.basic.web.frontcontroller.v1.controller;
+
+import hello.servlet.basic.web.frontcontroller.v1.ControllerV1;
+
+import javax.servlet.RequestDispatcher;
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+
+public class MemberFormControllerV1 implements ControllerV1 {
+
+    @Override
+    public void process(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+        String viewPath = "/WEB-INF/views/new-form.jsp";
+        RequestDispatcher dispatcher = request.getRequestDispatcher(viewPath);
+        dispatcher.forward(request, response); // 서버 내부에서 다시 호출(리다이렉트 X)
+    }
+
+}

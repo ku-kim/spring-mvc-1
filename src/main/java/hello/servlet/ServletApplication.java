f3f316f6465a24eb3b5c8391129fcf62aa2fec09
“kukim” <kukim.dev@gmail.com>
2022-03-18 15:49:03 +0900

    5.스프링MVC구조이해: 스프링 MVC 시작하기 (@Controller + @RequestMapping) 사용
    - 스프링 MVC의 애노테이션을 사용해 간단하게 Controller를 사용한다.
    - Controller에서 ModelAndView를 리턴하여 사용하고있다.
    - @Controller 애노테이션은 내부 @Component를 가지고 있다. @Controller 애노테이션 없이 @Component + @RequestMapper(url 없이 컨트롤러에만) 해도 RequestMappingHandlerMapping이 자동으로 인식한다. 또는 e.g. SpringMemberFormControllerV1를 핸들러 매핑이 조회하도록 직접 스프링 빈을 등록해줘도 된다.

--- src/main/java/hello/servlet/ServletApplication.java
+++ src/main/java/hello/servlet/ServletApplication.java
@@ -1,5 +1,6 @@
 package hello.servlet;
 
+import hello.servlet.basic.web.springmvc.v1.SpringMemberFormControllerV1;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.web.servlet.ServletComponentScan;
@@ -13,6 +14,11 @@ public class ServletApplication {
 	public static void main(String[] args) {
 		SpringApplication.run(ServletApplication.class, args);
 	}
+
+//	@Bean // SpringMemberFormControllerV1에서 @Controller 안해도 직접 스프링 빈 등록하면 찾을 수 있음
+//	SpringMemberFormControllerV1 springMemberFormControllerV1() {
+//		return new SpringMemberFormControllerV1();
+//	}
 
 //	@Bean
 //	InternalResourceViewResolver internalResourceViewResolver() {

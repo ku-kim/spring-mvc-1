75d337012e389c48b19e07b81bd4283a07a5f7c9
“kukim” <kukim.dev@gmail.com>
2022-03-18 11:40:28 +0900

    MVC프레임워크 만들기: 유연한컨트롤러1(v5)
    - v4는 다양한 종류의 컨트롤러 사용이 어려웠다.
    - 중간에 어댑터 역할을 하는 '핸들러 어댑터'를 두어 개발자 입장에서 편하게 다양한 종류의 어댑터를 사용한다.
    - 현재 ControllerV3만 구현되어 있어 장점을 못느끼겠지만 유연한컨트롤러2 커밋을 마저 살펴보자.

---
+++ src/main/java/hello/servlet/basic/web/frontcontroller/v5/FrontControllerServletV5.java
@@ -0,0 +1,79 @@
+package hello.servlet.basic.web.frontcontroller.v5;
+
+import hello.servlet.basic.web.frontcontroller.ModelView;
+import hello.servlet.basic.web.frontcontroller.MyView;
+import hello.servlet.basic.web.frontcontroller.v3.controller.MemberFormControllerV3;
+import hello.servlet.basic.web.frontcontroller.v3.controller.MemberListControllerV3;
+import hello.servlet.basic.web.frontcontroller.v3.controller.MemberSaveControllerV3;
+import hello.servlet.basic.web.frontcontroller.v4.controller.MemberFormControllerV4;
+import hello.servlet.basic.web.frontcontroller.v4.controller.MemberListControllerV4;
+import hello.servlet.basic.web.frontcontroller.v4.controller.MemberSaveControllerV4;
+import hello.servlet.basic.web.frontcontroller.v5.adapter.ControllerV3HandlerAdapter;
+
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+import java.util.*;
+
+@WebServlet(name = "frontControllerServletV5", urlPatterns = "/front-controller/v5/*")
+public class FrontControllerServletV5 extends HttpServlet {
+
+    private final Map<String, Object> handlerMappingMap = new HashMap<>();
+    private final List<MyHandlerAdapter> handlerAdapterList = new ArrayList<>();
+
+    public FrontControllerServletV5() {
+        initHandlerMappingMap();
+        initHandlerAdapters();
+    }
+
+    private boolean initHandlerAdapters() {
+        return handlerAdapterList.add(new ControllerV3HandlerAdapter());
+    }
+
+    private void initHandlerMappingMap() {
+        handlerMappingMap.put("/front-controller/v5/v3/members/new-form", new MemberFormControllerV3());
+        handlerMappingMap.put("/front-controller/v5/v3/members/save", new MemberSaveControllerV3());
+        handlerMappingMap.put("/front-controller/v5/v3/members", new MemberListControllerV3());
+    }
+
+    @Override
+    protected void service(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+        System.out.println("FrontControllerServletV5.service");
+
+        Object handler = getHandler(request);
+
+        if (handler == null) {
+            response.setStatus(HttpServletResponse.SC_NOT_FOUND);
+            return;
+        }
+
+        MyHandlerAdapter adapter = getHandlerAdapter(handler);
+
+        ModelView mv = adapter.handle(request, response, handler);
+        String viewName = mv.getViewName();// 논리이름 e.g. new-form -> "/WEB-INF/views/newform 으로 바꿔줘야함 - viewResolve
+        MyView view = viewResolver(viewName);
+        view.render(mv.getModel(), request, response);
+    }
+
+    private MyView viewResolver(String viewName) {
+        return new MyView("/WEB-INF/views/" + viewName + ".jsp");
+    }
+
+    private MyHandlerAdapter getHandlerAdapter(Object handler) {
+        for (MyHandlerAdapter adapter : handlerAdapterList) {
+            if (adapter.supports(handler)) {
+                return adapter;
+            }
+        }
+        throw new IllegalArgumentException("handler adapter를 찾을 수 없습니다 " + handler);
+//        Optional<MyHandlerAdapter> adapter = handlerAdapterList.stream().filter(adapter -> adapter.supports(handler)).findAny();
+    }
+
+    private Object getHandler(HttpServletRequest request) {
+        String requestURI = request.getRequestURI();
+        return handlerMappingMap.get(requestURI);
+    }
+}
